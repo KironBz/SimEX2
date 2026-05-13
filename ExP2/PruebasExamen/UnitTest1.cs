c38c2c4c8598ba5947908e128a3ba7fc929486c4
Alumnos <Alumnos@L19-2543309>
2026-05-13 09:14:49 -0600

    ññ

--- ExP2/PruebasExamen/UnitTest1.cs
+++ ExP2/PruebasExamen/UnitTest1.cs
@@ -5,7 +5,7 @@ namespace PruebasExamen
         [Fact]
         public void Test1()
         {
-            // apartado para pruebas cddbb
+            // apartado para pruebas cddbblll
         }
     }
 }
