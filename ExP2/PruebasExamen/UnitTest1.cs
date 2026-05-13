0e20db62d75ac0768c7e7aed66411b0f8452650a
Alumnos <Alumnos@L19-2543309>
2026-05-13 09:15:55 -0600

    ppp

--- ExP2/PruebasExamen/UnitTest1.cs
+++ ExP2/PruebasExamen/UnitTest1.cs
@@ -5,7 +5,7 @@ namespace PruebasExamen
         [Fact]
         public void Test1()
         {
-            // apartado para pruebas cddbblll
+            // apartado para pruebas cddbblllññññ
         }
     }
 }
